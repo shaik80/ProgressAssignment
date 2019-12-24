cd94307e220fc82eb642e7013f892bcd5314f699
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-12-24 02:05:43 -0800

    react and redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import './App.css';
+import React,{Fragment,useEffect} from 'react';
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        hello
-      </header>
-    </div>
-  );
-}
 
+const App = () => {
+return (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar/>
+          <Switch>
+            {/* <Route exact path="/regisration" component={registration}/>
+            <Route exact path="/login" component={login}/>
+            <Route exact path="/" component={Loader}/> */}
+          </Switch>
+      </Fragment>
+    </Router>
+  </Provider>
+)};
 export default App;
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,19 +1,8 @@
 import React,{Fragment} from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar , Nav , NavDropdown} from 'react-bootstrap';
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { logout } from "../../actions/auth";
 
-const navbar = ({auth:{isAuthenticated,loading},logout}) => {
-  const authLink = (
-    <Nav>
-    <Nav.Link onClick={logout} href="#">Logout</Nav.Link>
-  </Nav>
-  );
-  const geustLinks =(
-    
-  );
+import { Navbar , Nav , NavDropdown} from 'react-bootstrap';
+
+const navbar = () => {
     return (
 <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Navbar.Brand href="#home">Apna delivery</Navbar.Brand>
@@ -27,18 +16,8 @@ const navbar = ({auth:{isAuthenticated,loading},logout}) => {
         Rgistration
       </Nav.Link>
     </Nav>
-    
-
   </Navbar.Collapse>
 </Navbar>
     )
 };
-navbar.prototype = {
-  logout: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-}
-
-const mapStateToProps = state =>({
-  auth: state.auth
-});
-export default connect(mapStateToProps,{ logout })(navbar)
+export default navbar
